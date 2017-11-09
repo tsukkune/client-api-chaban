699be70057ecbf4045eb2c89b28fc43f65027b17
Unknown <nico.depero@hotmail.fr>
2017-11-09 16:26:12 +0100

    verification search

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,15 +15,23 @@ class HomePage extends Component {
     this.handleSetInfo = this.handleSetInfo.bind(this)
   }
 
-  handleSetInfo=(value1,value2)=>{
-    this.setState({
-      receiveData:[]
-    })
-    console.log('from: '+this.state.from+" value1: "+value1)
-    fetch('http://localhost:1337/?from='+value1+'&to='+value2)
-    .then(res=>res.json())
-    .then(data=>this.setState({receiveData:data}),data=>{this.setState({receiveData:[]});alert('erreur api non disponible')})
-    .catch(e=>console.log(e))
+  handleSetInfo=(from,to)=>{
+    let regex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+    if(regex.test(from) && regex.test(to)){
+      let date1 = from.split("-")
+      let date2 = to.split("-")
+      if(new Date(date1[2]+"/"+date1[1]+"/"+date1[0]).getTime() > new Date(date2[2]+"/"+date2[1]+"/"+date2[0]).getTime()){
+        alert('from superieur a to')
+      }else{
+        this.setState({
+          receiveData:[]
+        })
+        fetch('http://localhost:1337/?from='+from+'&to='+to)
+        .then(res=>res.json())
+        .then(data=>this.setState({receiveData:data}),data=>{this.setState({receiveData:[]});alert('erreur api non disponible')})
+        .catch(e=>console.log(e))
+      }
+    }
   }  
 
   componentDidMount(){
